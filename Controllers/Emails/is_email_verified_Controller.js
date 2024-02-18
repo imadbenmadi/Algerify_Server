666e17b0cc47ad735f648151cc82ab0c12cffc31
imadbenmadi <imadbenmadi891@gmail.com>
2024-02-18 15:11:08 +0100

    Add authentication check and token refresh logic

--- Controllers/Emails/is_email_verified_Controller.js
+++ Controllers/Emails/is_email_verified_Controller.js
@@ -4,6 +4,21 @@ const handle_check = async (req, res) => {
     try {
         const userId = req.params.userId;
         if (!userId) return res.status(404).json({ error: "messing Data" });
+        else {
+            const isAuth = await Verify_user(req, res);
+            if (isAuth.status == false)
+                return res
+                    .status(401)
+                    .json({ error: "Unauthorized: Invalid token" });
+            if (isAuth.status == true && isAuth.Refresh == true) {
+                res.cookie("accessToken", isAuth.newAccessToken, {
+                    httpOnly: true,
+                    sameSite: "None",
+                    secure: true,
+                    maxAge: 60 * 60 * 1000, // 10 minutes in milliseconds
+                });
+            }
+        }
         const user = await Users.findById(userId).select("IsEmailVerified");
         if (!user) {
             return res.status(404).json({ error: "User Not Found" });
