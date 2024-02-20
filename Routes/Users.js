95b85fc0a3c5af98eac930e07fe31d0748a1b4c7
imadbenmadi <imadbenmadi891@gmail.com>
2024-02-20 19:28:41 +0100

    refactoring User Apis

--- Routes/Users.js
+++ Routes/Users.js
@@ -4,34 +4,50 @@ const UserController = require("../Controllers/UserController");
 const RateController = require("../Controllers/RateController");
 const CommentController = require("../Controllers/CommentController");
 router.get("/", UserController.getAllUsers); // Only Admin
-router.get("/Profile/:userId", UserController.getProfile); // Only Admin
-router.get("/:userId", UserController.getUser); 
+router.get("/:userId/Profile", UserController.getProfile); // Only Admin
+router.get("/:userId", UserController.getUser);
 
-router.put("/:userId", UserController.EditProfile); 
+router.put("/:userId", UserController.EditProfile);
 router.delete("/:userId", UserController.DeleteProfile); // both Admin and User
 
-
-router.post("/:userid/Basket/:productId", UserController.add_to_Basket);
-router.delete("/:userid/Basket/:productId", UserController.delete_from_Basket);
-router.get("/Basket/:userId", UserController.get_Basket);
-router.post("/:userid/Favorit/:productId", UserController.add_to_Favorit);
-router.delete("/:userid/Favorit/:productId", UserController.delete_from_Favorit);
-router.get("/Favorit/:userId", UserController.get_Favorite);
-router.post("/:userid/RateProduct/:productId", RateController.RateProduct);
-router.delete("/:userid/RateProduct/:userId", RateController.Delete_RateProduct);
-router.post("/CommentProduct/:userId", CommentController.CommentProduct);
+router.post("/:userId/Basket/:productId", UserController.add_to_Basket);
+router.delete("/:userId/Basket/:productId", UserController.delete_from_Basket);
+router.get("/:userId/Basket", UserController.get_Basket);
+router.post("/:userId/Favorit/:productId", UserController.add_to_Favorit);
 router.delete(
-    "/CommentProduct/:userId",
+    "/:userId/Favorit/:productId",
+    UserController.delete_from_Favorit
+);
+router.get("/:userId/Favorit", UserController.get_Favorite);
+router.post("/:userId/RateProduct/:productId", RateController.RateProduct);
+router.delete(
+    "/:userId/RateProduct/:productId",
+    RateController.Delete_RateProduct
+);
+router.post(
+    "/:userId/CommentProduct/:productId",
+    CommentController.CommentProduct
+);
+router.delete(
+    "/:userId/CommentProduct/:productId",
     CommentController.Delete_CommentProduct
 );
-router.get("/get_user_Rate/:productId", RateController.get_product_userRate);
-router.get("/get_user_Comment/:productId", CommentController.get_product_userComment);
+router.get(
+    "/:userId/get_user_Rate/:productId",
+    RateController.get_product_userRate
+);
+router.get(
+    "/:userId/get_user_Comment/:productId",
+    CommentController.get_product_userComment
+);
 
-router.get("/get_user_Rate/:storeId", RateController.get_Store_userRate);
-router.post("/RateStore/:userId", RateController.RateStore);
-router.delete("/RateStore/:userId", RateController.Delete_RateStore);
+router.get(
+    "/:userId/get_user_Rate/:storeId",
+    RateController.get_Store_userRate
+);
+router.post("/:userId/RateStore/:userId", RateController.RateStore);
+router.delete("/:userId/RateStore/:userId", RateController.Delete_RateStore);
 
 router.post("/:userId/CreateStore", UserController.CreateStore);
-
 
 module.exports = router;
